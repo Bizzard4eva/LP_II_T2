552eb2b215fbdd6ebab44297576a82d7edeb1901
Bizzard <jevbxx@gmail.com>
2025-02-09 04:29:22 -0500

    Login creado

---
+++ src/main/java/Controlador/ControladorProveedor.java
@@ -0,0 +1,27 @@
+package Controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ControladorProveedor extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ControladorProveedor() {
+        super();
+        // TODO
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO
+		doGet(request, response);
+	}
+
+}
